944c003094c7ee2a9f48c63874c63363aee8ba93
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-09-17 23:41:50 +0530

    Complete project

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 import streamlit as st
 import imgtext
-import prompt_generator as pg
 import story
 import text2image as textimg
 import storyvoice as sv
 
 
-def main():
-    st.set_page_config(page_title="Imagetory", page_icon="🖥️")
-    uploaded_file = st.file_uploader("Choose a file")
+def c1f():
+    uploaded_file = st.file_uploader("Upload an Image")
     if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+        st.image(image=uploaded_file, caption="Uploaded Image",
+                 use_column_width=True)
 
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+        Description = imgtext.query(uploaded_file.name, st.secrets["hf"])
+        Story = story.story(Description, st.secrets["openai"])
+        sv.query(Story, st.secrets["hf"])
 
-        string_data = stringio.read()
-        st.write(string_data)
+        with st.expander("Description"):
+            st.write(Description)
+        with st.expander("Story"):
+            st.write(Story)
+        st.audio("audio.mp3")
 
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+
+def c2f():
+    prompt = st.text_input('Enter The Image Prompt',
+                           placeholder="Ex: kittens with cat")
+    if prompt != "":
+        result = textimg.query(prompt, st.secrets["hf"])
+        st.image(result, caption=prompt, use_column_width=True)
+
+
+def main():
+    st.set_page_config(page_title="Imagetory", page_icon="🖥️")
+    st.title(":orange[Imagetory]📷: The story of an image")
+    st.markdown(
+        "A unique way to bring **photographs to life**, delving into the emotions, characters, and environments within them.")
+    option = st.selectbox(
+        'Select your way',
+        ('NONE ❌', 'IMAGE TO STORY ✔️', 'TEXT TO IMAGE ✔️'))
+
+    st.write('You selected:', option)
+    if (option == 'IMAGE TO STORY ✔️'):
+        c1f()
+    if (option == 'TEXT TO IMAGE ✔️'):
+        c2f()
+
+
+if __name__ == '__main__':
+    main()
--- text2image.py
+++ text2image.py
@@ -1,19 +1,9 @@
-from PIL import Image
-import io
 import requests
 
 
 def query(payload, api):
     API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
     headers = {"Authorization": f"Bearer {api}"}
-    response = requests.post(API_URL, headers=headers, json=payload)
+    payload1 = dict(inputs=payload)
+    response = requests.post(API_URL, headers=headers, json=payload1)
     return response.content
-
-
-image_bytes = query({
-    "inputs": 'tiger as astronutetus, illustration by tim burton',
-})
-
-image = Image.open(io.BytesIO(image_bytes))
-
-image.show()
