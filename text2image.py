21656d0b97d5763ffc4a85a392f1a7327b9815c0
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2023-09-18 16:06:16 +0530

    Update text2image.py

--- text2image.py
+++ text2image.py
@@ -2,7 +2,7 @@ import requests
 
 
 def query(payload, api):
-    API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+    API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
     headers = {"Authorization": f"Bearer {api}"}
     payload1 = dict(inputs=payload)
     response = requests.post(API_URL, headers=headers, json=payload1)
